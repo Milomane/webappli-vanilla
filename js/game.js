ba97ac5ec7beafa63e159a9f313aec379588180c
Milo <florentin.roberge@gmail.com>
2021-01-11 10:24:00 +0100

    Create loop

--- js/game.js
+++ js/game.js
@@ -1,1 +1,16 @@
-console.log("HA BAS SA MARCHE")
+const canvas = document.getElementById('canvasTest');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'cyan';
+ctx.fillRect(10, 10, 100, 100)
+
+let frame = 0;
+let variable = "Hello"
+
+
+function gameLoop() {
+    console.log("frame")
+    frame += 1
+}
+
+setInterval(gameLoop, 1000/60)
