72e919f0d2cbc07c082e7ca9bf288ce274efca7f
Milo <florentin.roberge@gmail.com>
2021-01-11 11:07:36 +0100

    Update site

--- js/game.js
+++ js/game.js
@@ -1,22 +1,55 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
-let x = 10;
+let x = 70;
+let y = 10;
+let squareHeight = 70;
+let squareWidht = 50;
+
+let xSpeed = -5;
+let ySpeed = -2;
 
 let frame = 0;
 let variable = "Hello"
 
 function gameLoop() {
-    console.log("frame")
+    
     frame += 1
 
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, 300, 300)
+    if (x + squareWidht + xSpeed >= canvas.clientWidth) {
+        console.log("x+")
+        x = canvas.clientWidth - squareWidht;
+        xSpeed = -2;
+    }
+    if (x + xSpeed <= 0) {
+        console.log("x-")
+        x = 0;
+        xSpeed = 2;
+    }
 
-    ctx.fillStyle = 'cyan';
-    ctx.fillRect(10, 10, 100, 100)
+    if (y + squareHeight + ySpeed >= canvas.clientHeight) {
+        console.log("y+")
+        y = canvas.clientHeight - squareHeight;
+        ySpeed = -2;
+    }
+    if (y + ySpeed <= 0) {
+        console.log("y-")
+        y = 0;
+        ySpeed = 2;
+    }
 
-    x += 2
+    x += xSpeed;
+    y += ySpeed;
+
+    ctx.fillStyle = 'white'
+    ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight)
+
+    ctx.fillStyle = 'cyan'
+    ctx.fillRect(x, y, squareWidht, squareHeight)
+}
+
+function moveSquares() {
+    
 }
 
 setInterval(gameLoop, 1000/60)
