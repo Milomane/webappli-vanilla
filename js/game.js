a98eec550693130911ff53bc044fcea2a086be2e
Milo <florentin.roberge@gmail.com>
2021-01-11 10:29:07 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,8 +1,9 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'cyan';
-ctx.fillRect(10, 10, 100, 100)
+let x = 10;
+
+
 
 let frame = 0;
 let variable = "Hello"
@@ -11,6 +12,14 @@ let variable = "Hello"
 function gameLoop() {
     console.log("frame")
     frame += 1
+
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, 300, 300)
+
+    ctx.fillStyle = 'cyan';
+    ctx.fillRect(10, 10, 100, 100)
+
+    x += 2
 }
 
 setInterval(gameLoop, 1000/60)
