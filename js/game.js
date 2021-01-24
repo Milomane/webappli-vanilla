e7efca0681ed8f81a42933c241cf267ab05b21c5
Milo <florentin.roberge@gmail.com>
2021-01-24 21:00:12 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -14,6 +14,9 @@ let xAcc = 0;
 let yAcc = 0;
 let zAcc = 0;
 
+let gravityScale = 1000;
+let massPerSurface = 5;
+
 let rectUseAcc = false;
 let circleUseAcc = true;
 
@@ -32,21 +35,24 @@ function rect_create(x, y, xSpeed, ySpeed, xSize, ySize, color, img) {
     return square
 }
 
-function circle_create(x, y, xSpeed, ySpeed, radius, color) {
+function circle_create(x, y, fx, fy, xSpeed, ySpeed, radius, colorValue) {
     let circle = {
         x: x,
         y: y,
+        fx: 0,
+        fy: 0,
         xSpeed: xSpeed,
         ySpeed: ySpeed,
         radius : radius,
-        color : color
+        colorValue : colorValue,
     };
     return circle
 }
 
 let rect = rect_create(200, 100, -2, 2, 100, 60, 'red', true)
 let rect1 = rect_create(100, 200, 4, 2, 30, 40, 'blue', false)
-let circle = circle_create(200, 200, 0, 0, 50, randomColor())
+let ci1 = circle_create(200, 200, 0, 0, 0, 0, 50, Math.floor(Math.random()*16777215))
+let ci2 = circle_create(100, 200, 0, 0, 50, Math.floor(Math.random()*16777215))
 
 let gameobjects = [
     rect,
@@ -54,7 +60,8 @@ let gameobjects = [
 ]
 
 let circleobjects = [
-    circle
+    ci1,
+    ci2
 ]
 
 function randomColor() {
@@ -106,6 +113,10 @@ function gameLoop() {
         moveSquares(square);
     });
 
+    //moveCircleWithGravity(60/1000, circleobjects);
+
+    //drawCircle(circleobjects[0])
+
     circleobjects.forEach(
     circle => {
         moveCircle(circle);
@@ -143,11 +154,10 @@ function handleEndClick(evt)
     let xPos = evt.offsetX;
     let yPos = evt.offsetY;
 
-    let color = randomColor();
+    let colorValue = Math.floor(Math.random()*16777215);
 
     //let newRect = cir(xPos, yPos, xSPeed, ySpeed, xSize, ySize, color, false)
-    let newCircle = circle_create(xPos, yPos, /*xSPeed, ySpeed,*/ 0, 0, xSize / 2, color, false)
-
+    let newCircle = circle_create(xPos, yPos, /*xSPeed, ySpeed,*/0, 0, 0, 0, xSize / 2, colorValue)
 
     //gameobjects.push(newRect)
     circleobjects.push(newCircle)
@@ -156,6 +166,59 @@ function handleEndClick(evt)
 function getRandomArbitrary(min, max) 
 {
     return Math.random() * (max - min) + min;
+}
+
+function moveCircleWithGravity(dt, co) {
+
+    console.log(dt);
+
+    for (let c of co) {
+        c.fx = 0;
+        c.fy = 0;
+    }
+    for (let[i, c1] of co.entries()) {
+        for (let[j, c2] of co.entries()) {
+            if (i < j) {
+                let dx = c2.x - c1.x;
+                let dy = c2.y - c1.y;
+                let r = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+                if (r < 1) {
+                    r = 1;
+                }
+
+                let f = (gravityScale * findCircleMass(c1) * findCircleMass(c2)) / Math.pow(r, 2);
+                let fx = f * dx / r;
+                let fy = f * dy / r;
+
+                c1.fx += fx;
+                c1.fy += fy;
+                c2.fx -= fx;
+                c2.fy -= fy;
+            }
+        }
+    }
+
+    for (let c1 of co) {
+        let ax = c1.fx / findCircleMass(c1);
+        let ay = c1.fy / findCircleMass(c1);
+
+        c1.xSpeed += ax * dt;
+        c1.ySpeed += ay * dt;
+
+        c1.x += c1.xSpeed * dt;
+        c1.y += c1.ySpeed * dt;
+
+        
+
+        ctx.fillStyle = c1.color;
+        ctx.beginPath();
+        ctx.arc(c1.x, c1.y, c1.radius, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
+
+function findCircleMass(circle) {
+    return (Math.PI * Math.pow(circle.radius, 2) * massPerSurface)
 }
 
 function moveCircle(circle) {
@@ -199,8 +262,10 @@ function moveCircle(circle) {
         circle.y += circle.ySpeed;
     }
     
-    
-    ctx.fillStyle = circle.color;
+    circle.colorValue += 16777215;
+
+    ctx.fillStyle = "#" + circle.colorValue.toString(16);
+
     ctx.beginPath();
     ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
     ctx.fill();
@@ -253,6 +318,7 @@ function moveSquares(info) {
 
     if (!info.img) {
         ctx.fillStyle = info.color;
+        
         ctx.fillRect(info.x, info.y, info.xSize, info.ySize)
     }
     else 
